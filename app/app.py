ed9647acd87ea968f4ee41c911da0f67d949cf82
vineethsudhir <vineeth.sudhir@gmail.com>
2022-02-08 06:32:20 -0500

    changed name

--- app/app.py
+++ app/app.py
@@ -1,13 +1,11 @@
 """A simple flask web app"""
-from flask import Flask, render_template
-from controllers.index_controller import IndexController
+from flask import Flask
 from werkzeug.debug import DebuggedApplication
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 app.wsgi_app = DebuggedApplication(app.wsgi_app, True)
 
-
-@app.route("/", methods=['GET'])
-def index_get():
-    return IndexController.get()
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World! From Vinith</p>"
--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "kwilliam"
+        name = "Vinith Sudhir"
         return render_template('index.html', name=name)
